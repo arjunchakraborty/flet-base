f02fd70eba5cb9d891b46eaa5c93b207f1aaf379
tonyxdz <tony23@gmail.com>
2025-01-07 11:42:47 +0100

    create basic login interface and logic

--- pages/login.py
+++ pages/login.py
@@ -11,12 +11,40 @@ class LoginModel(Model):
     spacing = 10
 
     # UI Components
-    appbar = ft.AppBar(
-        title=ft.Text("Login"),
-        center_title=True,
-        bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST
+
+    username_field = ft.TextField(
+        label="Username",
+    )
+    password_field = ft.TextField(
+            label="Password", password=True, can_reveal_password=True
     )
 
+    #required_cred_dlg = 
+
     controls = [
-        ft.Text("Login Page", size=24),
+        ft.Text("Login", size=24),
+        ft.Column(
+                    controls=[
+                        username_field,
+                        password_field,
+                        ft.OutlinedButton(text="Login", on_click="request_login")
+                    ]
+                ),
     ]
+
+    def request_login(self, e):
+        
+        if self.username_field.value == "" or self.password_field.value == "":
+            #empty fields show error
+            self.controls += ft.AlertDialog(
+                            title=ft.Text("Username and Password required!", size=18),
+                            open=True,
+                        ),
+
+            self.update()
+        else:
+            #send request and handle response here
+            print("sending login request ...")
+            print("username : ", self.username_field.value)
+            print("password : ", self.password_field.value)
+            self.page.go('/home')
